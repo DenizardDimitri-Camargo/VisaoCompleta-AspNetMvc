e41706b0ff509901f1c68f699ec6ee74f0cc4fc1
DenizardDimitri-Camargo <62820018+DenizardDimitri-Camargo@users.noreply.github.com>
2021-01-30 17:39:12 -0400

    REPOSITÓRIOS: Entendendo o Repository Pattern
    EF
    Repositórios Genéricos //não recomendável
    Dispose
    Repository Pattern

--- BookStore/Controllers/AuthorController.cs
+++ BookStore/Controllers/AuthorController.cs
@@ -1,4 +1,7 @@
-﻿using BookStore.Filters;
+﻿using BookStore.Domain;
+using BookStore.Filters;
+using BookStore.Repositories;
+using BookStore.Repositories.Contracts;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,11 +14,19 @@ namespace BookStore.Controllers
     //[LogActionFilter()] //filtra em todas as actions
     public class AuthorController : Controller
     {
+        private IAuthorRepository repository;
+
+        public AuthorController()
+        {
+            repository = new AuthorRepository();
+        }
+
         [Route("listar")]
         //[LogActionFilter()] //filtra nesta action
         public ActionResult Index()
         {
-            return View();
+            var autores = repository.Get();
+            return View(autores);
         }
 
         [Route("criar")]
@@ -24,16 +35,48 @@ namespace BookStore.Controllers
             return View(); 
         }
 
+        [Route("criar")]
+        [HttpPost]
+        public ActionResult Create(Autor autor)
+        {
+            if (repository.Create(autor))
+                return RedirectToAction(nameof(Index));
+
+            return View(autor); //se der erro no if ele vem aqui
+        }
+
         [Route("editar/{id:int}")]
         public ActionResult Edit(int id)
         {
-            return View();
+            var author = repository.Get(id);
+            return View(author);
+        }
+
+        [Route("editar/{id:int}")]
+        [HttpPost]
+        public ActionResult Edit(Autor autor)
+        {
+            if (repository.Update(autor))
+                return RedirectToAction(nameof(Index));
+
+            return View(autor); //se der erro no if ele vem aqui
         }
 
         [Route("excluir/{id:int}")]
         public ActionResult Delete(int id)
         {
-            return View();
+            var author = repository.Get(id);
+            return View(author);
+        }
+
+        [Route("excluir/{id:int}")]
+        [HttpPost]
+        [ActionName("Delete")] //serve para manter a convenção de nome na url
+        public ActionResult DeleteConfirm(int id)
+        {
+            repository.Delete(id);
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
---
+++ BookStore/Repositories/Contracts/IAuthorRepository.cs
@@ -0,0 +1,18 @@
+﻿using BookStore.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookStore.Repositories.Contracts
+{
+    public interface IAuthorRepository : IDisposable //força ter um método Dispose quando for destruir a conexão com BD
+    {
+        List<Autor> Get();
+        Autor Get(int id);
+        List<Autor> GetByName(string name);
+        bool Create(Autor autor); //poderia ser void
+        bool Update(Autor autor); //poderia ser void
+        void Delete(int id);
+    }
+}

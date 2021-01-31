d75b392b29eec2ae2e7fc4b594ef2bfff0fdc388
DenizardDimitri-Camargo <62820018+DenizardDimitri-Camargo@users.noreply.github.com>
2021-01-30 23:13:17 -0400

    VIEWS:  Entendendo o @Html
    Formulários
    DropDownList

---
+++ BookStore/Controllers/LivroController.cs
@@ -0,0 +1,38 @@
+﻿using BookStore.Context;
+using BookStore.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookStore.Controllers
+{
+    [RoutePrefix("livros")]
+    public class LivroController : Controller
+    {
+        BookStoreDataContext _db = new BookStoreDataContext();
+
+        [Route("criar")]
+        public ActionResult Create()
+        {
+            var categorias = _db.Categorias.ToList();
+            var viewModel = new CreateBookViewModel
+            {
+                Titulo = "",
+                ISBN = "",
+                CategoriaId = 0,
+                Categoriaoptions = new SelectList(categorias, "Id", "Nome") //SelectList é um tipo chave/valor
+            };
+
+            return View(viewModel);
+        }
+
+        [Route("criar")]
+        [HttpPost]
+        public ActionResult Create(CreateBookViewModel model)
+        {
+            return View();
+        }
+    }
+}
---
+++ BookStore/ViewModels/CreateBookViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookStore.ViewModels
+{
+    public class CreateBookViewModel
+    {
+
+        [Required(ErrorMessage = "* Título Obrigatório")]
+        [Display(Name = "Nome do Livro")]
+        public string Titulo { get; set; } //nome
+
+        [Required(ErrorMessage = "* ISBN Obrigatório")]
+        public string ISBN { get; set; }
+
+        [Required(ErrorMessage = "* Data de Lançamento Obrigatória")]
+        [Display(Name = "Data de Lançamento")]
+        [DataType(DataType.Date)]
+        public DateTime DataLancamento { get; set; }
+        
+        [Required(ErrorMessage = "* Categoria Obrigatória")]
+        [Display(Name = "Nome do Livro")]
+        public int CategoriaId { get; set; } //não foi exibido na view, creio que seja só para relacionamento
+
+        public SelectList Categoriaoptions { get; set; }
+    }
+}
